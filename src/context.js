0a57a744e56aa9331c853ce39e22985e62c03ffe
spendrak <sonya7usekova@gmal.com>
2025-03-21 15:01:48 +0500

    фикс контекста

--- src/context.js
+++ src/context.js
@@ -2,6 +2,8 @@ import { createContext } from 'react';
 
 export const AppContext = createContext({
 	todos: [],
-	isLoading: false,
+	sortedTodos: () => {},
+	searchInfo: () => {},
+	dispath: () => {},
 	refreshTodoList: () => {},
 });
